a5cbf9919748bacec8bb300bbb4fb4d0c9c6c347
aby456 <adquillupangui@espe.edu.ec>
2022-05-18 13:03:04 +0200

    porfin1

---
+++ prestamo_coches/src/main/java/com/prestamo_coches/prestamo_coches/PrestamoCochesApplication.java
@@ -0,0 +1,13 @@
+package com.prestamo_coches.prestamo_coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamoCochesApplication.class, args);
+	}
+
+}
---
+++ prestamo_coches/src/test/java/com/prestamo_coches/prestamo_coches/PrestamoCochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestamo_coches.prestamo_coches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoCochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
